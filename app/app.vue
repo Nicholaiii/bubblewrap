2a261a5fed6b8682e76afbdf3759b73c6a7e0472
Nicholai Nissen <nicholainissen@disroot.org>
2025-01-26 12:13:54 +0100

    feat: icon?

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,16 @@
+<script lang="ts">
+useHead({
+  title: 'Bubblewrap Discord',
+  link: [
+    { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+  ],
+  meta: [{ name: 'apple-mobile-web-app-title', content: 'Bubblewrap Discord' }],
+  htmlAttrs: {
+    class: 'dark',
+  },
+})
+</script>
+
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLoadingIndicator />
